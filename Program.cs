9b8444399ec7535c81225c8b80f3e77f620b5a41
TRIXServer <admin@trixserver.com>
2022-05-21 18:49:10 -0300

    commit

--- Program.cs
+++ Program.cs
@@ -1,33 +1,3 @@
 ﻿Console.Clear();
+Console.WriteLine("--");
 Console.WriteLine("Hola Mundo");
-
-
-public class pixel
-{
-    private int x;
-    private int y;
-
-    public int X
-    {
-        get;
-        set;
-    }
-
-    public int Y
-    {
-        get;
-        set;
-    }
-
-    public void moverPunto (int nX, int nY)
-    {
-        x = nX;
-        y = nY;
-    }
-
-    public pixel()
-    {
-        x = 0;
-        y = 0;
-    }
-}
