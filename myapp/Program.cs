7696403acfa7d3d9f8effd3870a833a252ed0c7e
TRIXServer <admin@trixserver.com>
2022-05-24 16:51:42 -0300

    commit

--- myapp/Program.cs
+++ myapp/Program.cs
@@ -1,15 +1,3 @@
 ﻿using System;
 
 Console.Clear();
-Console.Write("Ingrese un numero: ");
-string numeroIngresado = Console.ReadLine();
-
-Console.WriteLine("El numero ingresado es: " + numeroIngresado);
-Console.Write("El numero invertido es: ");
-
-char[] numeroArray = numeroIngresado.ToCharArray();
-
-for (int i = numeroArray.Length; i > 0 ; i--)
-{
-    Console.Write(numeroArray[i - 1]);
-}
