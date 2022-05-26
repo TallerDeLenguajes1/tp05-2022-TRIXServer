dd1d15a1517e9e29b73298d100c20d449a058046
TRIXServer <admin@trixserver.com>
2022-05-26 18:49:41 -0300

    commit

--- myapp/Program.cs
+++ myapp/Program.cs
@@ -1,10 +1,61 @@
 ﻿using System;
 
-Console.Clear();
-Console.WriteLine("1. SUMA");
-Console.WriteLine("2. RESTA");
-Console.WriteLine("3. MULTIPLICACION");
-Console.WriteLine("4. DIVISION");
-Console.Write("Ingrese la operacion: ");
-int operacion = Convert.ToInt32(Console.ReadLine());
+namespace ejercicio2
+{
+    class Program
+    {
+        static void Main ()
+        {
+            int operacion = 5;
+            do
+            {
+                int resultado = 0;
+
+                Console.Clear();
+                Console.WriteLine("1. SUMA");
+                Console.WriteLine("2. RESTA");
+                Console.WriteLine("3. MULTIPLICACION");
+                Console.WriteLine("4. DIVISION");
+                Console.WriteLine("0. SALIR");
+                Console.WriteLine("--");
+                Console.Write("Ingrese la opcion: ");
+                operacion = int.Parse(Console.ReadLine());
+
+                if (operacion == 1 || operacion == 2 || operacion == 3 || operacion == 4)
+                {
+                    Console.Write("Ingrese el primer numero: ");
+                    int numero1 = int.Parse(Console.ReadLine());
+                    Console.Write("Ingrese el segundo numero: ");
+                    int numero2 = int.Parse(Console.ReadLine());
+
+                    if (operacion == 1)
+                    {
+                        resultado = numero1 + numero2;
+
+                    }
+                    if (operacion == 2)
+                    {
+                        resultado = numero1 - numero2;
+
+                    }
+                    if (operacion == 3)
+                    {
+                        resultado = numero1 * numero2;
+
+                    }
+                    if (operacion == 4 && numero2 != 0)
+                    {
+                        resultado = numero1 / numero2;
+
+                    }
+                    Console.WriteLine("--");
+                    Console.WriteLine("El resultado es: " + resultado);
+                    Console.Read();
+
+                }
+            } while (operacion != 0) ;
+        }
+    }
+}
+
 
