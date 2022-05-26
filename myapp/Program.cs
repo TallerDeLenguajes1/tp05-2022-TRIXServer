7714739635d6fd75a193cd3b255506d0f5070ac3
TRIXServer <admin@trixserver.com>
2022-05-26 17:53:24 -0300

    commit

--- myapp/Program.cs
+++ myapp/Program.cs
@@ -1,3 +1,10 @@
 ﻿using System;
 
 Console.Clear();
+Console.WriteLine("1. SUMA");
+Console.WriteLine("2. RESTA");
+Console.WriteLine("3. MULTIPLICACION");
+Console.WriteLine("4. DIVISION");
+Console.Write("Ingrese la operacion: ");
+int operacion = Convert.ToInt32(Console.ReadLine());
+
