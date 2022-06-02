1d2687f169af16035823b8017417190a4b4a1b85
TRIXServer <admin@trixserver.com>
2022-06-02 18:18:24 -0300

    commit

--- myapp/Program.cs
+++ myapp/Program.cs
@@ -7,12 +7,22 @@ namespace ejercicio4
         static void Main ()
         {
             string textoIngresado;
+            string textoIngresadoDos;
             Console.Clear();
             Console.WriteLine("--");
             Console.Write("Ingrese un texto: ");
             textoIngresado = Console.ReadLine();
-            Console.WriteLine("El texto ingresado es: " + textoIngresado);
-            Console.WriteLine("Primer caracter: " + textoIngresado[0]);
+            Console.WriteLine("--");
+            Console.WriteLine("El texto ingresado es: \t\t\t" + textoIngresado);
+            Console.WriteLine("El primer caracter: \t\t\t" + textoIngresado[0]);
+            Console.WriteLine("El largo de la cadena ingresada es: \t" + textoIngresado.Length);
+            Console.WriteLine("--");
+            Console.Write("Ingrese un segundo texto: ");
+            textoIngresadoDos = Console.ReadLine();
+            Console.WriteLine("--");
+            Console.WriteLine("El texto ingresado es: \t\t\t" + textoIngresadoDos);
+            Console.WriteLine("Ambos textos concatenados: \t\t" + textoIngresado + textoIngresadoDos);
+
             Console.Write("Presione una tecla");
             Console.Read();
         }
