16b8da148992732ac28c5bd0b5b270a962142a50
TRIXServer <admin@trixserver.com>
2022-06-02 17:50:58 -0300

    commit

--- myapp/Program.cs
+++ myapp/Program.cs
@@ -1,42 +1,12 @@
 ﻿using System;
 
-namespace ejercicio3
+namespace ejercicio4
 {
     class Program
     {
         static void Main ()
         {
-            float numero = 0;
-            int numero1 = 0;
-            int numero2 = 0;
 
-            Console.Clear();
-            Console.WriteLine("--");
-            Console.Write("Ingrese un numero: ");
-            numero = float.Parse(Console.ReadLine());
-
-            Console.WriteLine("--");
-            Console.WriteLine($"El numero ingresado es \t{numero}");
-            Console.WriteLine($"El valor Absoluto es: \t" + Math.Abs(numero));
-            Console.WriteLine($"El cuadrado es: \t" + Math.Pow(numero, 2));
-            Console.WriteLine($"La raiz cuadrada es: \t" + Math.Sqrt(numero));
-            Console.WriteLine($"El seno es: \t\t" + Math.Sin(numero));
-            Console.WriteLine($"El coseno es: \t\t" + Math.Cos(numero));
-            Console.WriteLine($"La parte entera es: \t" + Math.Truncate(numero));
-
-            Console.WriteLine("--");
-            Console.Write("Ingrese el primer numero: ");
-            numero1 = int.Parse(Console.ReadLine());
-            Console.Write("Ingrese el segundo numero: ");
-            numero2 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("--");
-
-            Console.WriteLine($"El maximo de los dos es: \t" + Math.Max(numero1, numero2));
-            Console.WriteLine($"El minimo de los dos es: \t" + Math.Min(numero1, numero2));
-
-            Console.Write("Presione una tecla...");
-            Console.Read();
         }
     }
 }
